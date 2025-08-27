196192f0fb04a29005c4c045613859a376a33eef
ParampPathak <paramppathak@gmail.com>
2025-08-27 15:41:45 -0500

    Program

--- Practice_Project/practice_1.c
+++ Practice_Project/practice_1.c
@@ -6,3 +6,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+
+
+int isgoat(int x)
+{
+    return x + 3;
+}
+
+
+
+int main(void)
+{   
+    int x;
+
+    printf("Enter the value for x\n");
+
+    scanf("%d",&x);
+
+    x = isgoat(x);
+
+    printf("%d\n",x);
+   
+
+    if (x < 0)
+    {
+        printf("You are the goat throat");
+    }
+       
+    else
+    {
+    while(5)
+    {
+        printf("I am the goat");
+        if (x < 1)
+        {
+            return 5;
+        }
+    }
+
+    }
+   
+     return 0; 
+   
+    }
+
